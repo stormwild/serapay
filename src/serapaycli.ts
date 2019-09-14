f8e0953b7e5165e41c2a27b92ba60f3021b663e1
Alexander Torrijos <artorrijos@gmail.com>
2019-09-14 13:05:44 +0800

    Refactor serapaycli

--- src/serapaycli.ts
+++ src/serapaycli.ts
@@ -4,6 +4,17 @@ import Config, { IConfig } from './config';
 // tslint:disable-next-line: import-name
 import Serapay from './serapay';
 
+const runner = (path: string) => {
+  fs.readFile(path, 'utf8', async (err, data) => {
+    if (err) throw err;
+    const config: IConfig = await new Config().getConfig();
+    const serapay: Serapay = new Serapay();
+    const commissions: number[] = serapay.getCommissions(data, config);
+    // tslint:disable-next-line: no-console
+    commissions.forEach(c => console.log(c.toFixed(2)));
+  });
+};
+
 if (!module.parent) {
   // Called from the commandline
   const path = process.argv.slice(2);
@@ -11,13 +22,8 @@ if (!module.parent) {
     // tslint:disable-next-line: no-console
     console.log('Please provide a path to an input file!');
   } else {
-    fs.readFile(path[0], 'utf8', async (err, data) => {
-      if (err) throw err;
-      const config: IConfig = await new Config().getConfig();
-      const serapay: Serapay = new Serapay();
-      const commissions: number[] = serapay.getCommissions(data, config);
-      // tslint:disable-next-line: no-console
-      commissions.forEach(c => console.log(c.toFixed(2)));
-    });
+    runner(path[0]);
   }
 }
+
+export default runner;
